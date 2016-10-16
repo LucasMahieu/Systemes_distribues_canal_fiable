19cbb25bf336ba23d1653dbfadc030bf80d4e2ed
LucasMahieu <lucas.mahieu@grenoble-inp.org>
2016-10-16 18:08:27 +0200

    lien entre proc A et canal OK, + fichier de test

--- canal/src/main.c
+++ canal/src/main.c
@@ -9,16 +9,17 @@
 #include <sys/socket.h>
 #include <unistd.h> // sleep during x s
 
-
 #define SERVER "127.0.0.1"
 #define BUFLEN 512  		//Max length of buffer
 #define BUFDELLEN 2048	 	//Max length buffer for storing messages (and wait for them to complete before delivering them)
 #define PORT 8888   		//The port on which to listen for incoming data
 
+#define DEBUG
+
 typedef struct sockaddr_in Sockaddr_in;
 typedef int Socket;
 
-void die(char *s)
+void bug(char *s)
 {
 	perror(s);
 	exit(1);
@@ -30,24 +31,18 @@ Wait for the message "initialization" and reply "initialization".
 int handshakeServer(Socket s) {
 	Sockaddr_in si_other;
 	char* message = "initialization";
-	int recv_len, slen=10;  // Initialization at 10, do not understand why it does not work otherwise
+	unsigned int recv_len, slen=10;  // Initialization at 10, do not understand why it does not work otherwise
 	char buf[BUFLEN];
 	memset(buf,'\0', BUFLEN);
 
 	while (strcmp(buf, message)) {
-		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
-		{
-			die("recvfrom()");
-		}
+		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1) bug("recvfrom()");
 	}
 	printf("The server received the message : %s\n", buf);
 	//printf("oo, %d, %d", slen, sizeof(si_other));
 	
 	//now reply the client with "initialization"
-	if (sendto(s, message, sizeof(message), 0, (struct sockaddr*) &si_other, sizeof(si_other)) == -1)
-	{
-		die("sendto()");
-	}
+	if (sendto(s, message, sizeof(message), 0, (struct sockaddr*) &si_other, sizeof(si_other)) == -1) bug("sendto()");
 	return 0;
 }
 
@@ -58,22 +53,16 @@ Send the message "initialization" and wait for the answer "initialization".
 int handshakeClient(Socket s, Sockaddr_in* si_other_p) {
 	Sockaddr_in si_garbage;
 	char* message = "initialization";
-	int recv_len, slen;
+	unsigned int recv_len, slen;
 	char buf[BUFLEN];
 	memset(buf,'\0', BUFLEN);
 
 	while (strcmp(buf, message)) {
-		if (sendto(s, message, strlen(message), 0, (struct sockaddr*) si_other_p, sizeof(*si_other_p)) == -1)
-		{
-			die("sendto()");
-		}
+		if (sendto(s, message, strlen(message), 0, (struct sockaddr*) si_other_p, sizeof(*si_other_p)) == -1) bug("sendto()");
 		printf("Trying to connect ... \n");
 		// sleep(1);		//sleep function does not work
 
-		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_garbage, &slen)) == -1)
-		{
-			die("recvfrom()");
-		}
+		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_garbage, &slen)) == -1) bug("recvfrom()");
 	}
 	printf("The server received the message : %s \nThe communication can now begin\n", buf);
 	//printf("oo, %d, %d", slen, sizeof(si_other));
@@ -88,16 +77,13 @@ int main(int argc, char **argv)
 {
 	Sockaddr_in si_other;
 	Socket s;
-	int slen = sizeof(si_other) , recv_len;
+	unsigned int slen = sizeof(si_other) , recv_len;
 	char buf[BUFLEN];
 	char message[BUFLEN];
 
 
 	//create a UDP socket
-	if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-	{
-		die("socket");
-	}
+	if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) bug("socket");
 
 	if(argc <2){
 		printf("Enter the number of the canal: 0 for server, 1 pour client");
@@ -116,14 +102,10 @@ int main(int argc, char **argv)
 		si_me.sin_addr.s_addr = htonl(INADDR_ANY);
 
 		//bind socket to port
-		if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
-		{
-			die("bind");
-		}
+		if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1) bug("bind");
 
 		// Wait for the initialization message
-		handshakeServer(s);
-
+		//handshakeServer(s);
 
 		//keep listening for data
 		while(1)
@@ -134,20 +116,15 @@ int main(int argc, char **argv)
 			//Il faudra ici, bien vider le buffer = écrire un '\0' au début
 			//Sinon quand le message est plus petite que l'ancien, on voit encore l'ancien
 			//try to receive some data, this is a blocking call
-			if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
-			{
-				die("recvfrom()");
-			}
+			if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1) bug("recvfrom()");
 
 			//print details of the client/peer and the data received
 			printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
 			printf("Data: %s\n" , buf);
 
 			//now reply the client with the same data
-			if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)
-			{
-				die("sendto()");
-			}
+			if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1) bug("sendto()");
+			memset(buf,'\0', BUFLEN);
 		}
 		close(s);
 	}
@@ -164,41 +141,34 @@ int main(int argc, char **argv)
 			exit(1);
 		}
 		// Init the connection by sending a message and waiting for an answer
-		handshakeClient(s, &si_other);
+		//handshakeClient(s, &si_other);
 
-		while(1)
-		{
-			pid_t receive_pid;
-			int receive_status;
-			receive_pid = fork();
-			if(receive_pid == 0){
-				// Processus de reception des messages
+		pid_t receive_pid;
+		int receive_status;
+		receive_pid = fork();
+		// Processus de reception des messages
+		if(receive_pid == 0){
 
-			}else{
+		}else{  // Processus d'envoi des messages
+			while(1){
+#ifdef DEBUG
 				printf("Enter message : ");
+#endif
 				gets(message);
-
+				printf("Le fils (%d) a lu : %s \n", getpid(), message);
 				//send the message
-				if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1)
-				{
-					die("sendto()");
-				}
+				if (sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen)==-1) bug("sendto()");
 
 				//receive a reply and print it
 				//clear the buffer by filling null, it might have previously received data
 				memset(buf,'\0', BUFLEN);
 				//try to receive some data, this is a blocking call
-				if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1)
-				{
-					die("recvfrom()");
-				}
-
+				if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1) bug("recvfrom()");
 				puts(buf);
 			}
-			close(s);
-			
-			wait(&receive_status);
 		}
+		close(s);
+		wait(&receive_status);
 	}
 	return 0;
 }
