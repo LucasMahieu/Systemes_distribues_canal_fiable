035e494dfb63d2f750c05087efc321fd44c3a68d
LucasMahieu <lucas.mahieu@grenoble-inp.org>
2016-11-11 21:09:47 +0100

    ajout du ficher d'update du timeout

---
+++ canal/src/window.c
@@ -0,0 +1,19 @@
+/*
+ * ======================================================================
+ *         Author:  Lucas MAHIEU (), lucas.mahieu@grenoble-inp.org
+ * ======================================================================
+ */
+
+#include "window.h"
+#include <sys/time.h>
+
+void update_timeout(WaitAckElement* e, Time* t){
+	gettimeofday(t,NULL);
+	if(t->tv_usec + TIMEOUT_WAIT_ACK > MAX_TV_USEC){
+		e->timeout.tv_sec =  t->tv_sec + 1;
+		e->timeout.tv_usec = (t->tv_usec + TIMEOUT_WAIT_ACK)%MAX_TV_USEC;
+	}else{
+		e->timeout.tv_sec =  t->tv_sec;
+		e->timeout.tv_usec =  t->tv_usec + TIMEOUT_WAIT_ACK;
+	}
+}
